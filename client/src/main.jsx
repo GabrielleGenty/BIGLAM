c3f05810e3db2a3ab73c3d501606aeb1288abc1f
GabrielleGenty <gabrielle.genty@lapost.net>
2024-08-14 20:53:30 +0200

    update main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,16 +6,18 @@ import App from './App.jsx';
 import "./assets/styles/main.scss";
 import { UserProvider } from "./store/user/Context.jsx";
 import { CartProvider } from './store/cart/Context.jsx';
-//import { MenuProvider } from "./store/menu/Context.jsx"
+import { MenuProvider } from "./store/menu/Context.jsx"
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <CartProvider>
+      <MenuProvider>
         <UserProvider>
           <App />
         </UserProvider>
+      </MenuProvider> 
     </CartProvider>
   </BrowserRouter>
 
