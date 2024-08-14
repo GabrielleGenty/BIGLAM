b34d14251cd33835512ce99422642718bbbb212f
GabrielleGenty <gabrielle.genty@lapost.net>
2024-08-14 15:46:02 +0200

    update admin home

--- client/src/views/Admin/AdminHome.jsx
+++ client/src/views/Admin/AdminHome.jsx
@@ -1,18 +1,41 @@
-import { Link } from "react-router-dom";
+
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faPowerOff} from '@fortawesome/free-solid-svg-icons';
+
+import { useUser } from "../../hooks/UseUser";
 
 
 // import useMenu from "../../hooks/UseMenu";
 
 
 function AdminHome() {
+  const  {logout} = useUser();
+  const { user, setUser } = useUser();
+  async function logoutHandler(){
+      const response = await fetch(
+          "http://localhost:9000/api/v1/users/logout",
+          {
+            method: "GET",
+          credentials: "include",
+      });
+      if (response.ok){
+          setUser(null);
+          // toggleMenu();
+      }
+  } 
 
 
   //UseMeu();
 
   return (
     <main>
+       <header>
+        <h1>BACK OFFICE</h1>
+        <button onClick={logoutHandler}><FontAwesomeIcon icon={faPowerOff} /></button>
+    </header>
       <section>
-      <h1>BACK OFFICE</h1>
+      <h2>ADMIN PANEL</h2>
       <nav>
       <Link to="/users"> <strong>Utilisateurs</strong></Link>
       <Link to="/products"><strong>Produits</strong></Link>
