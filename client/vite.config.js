77d4fa4c057be17b6067ff22f68b9977876d4274
GabrielleGenty <gabrielle.genty@lapost.net>
2024-08-25 09:41:28 +0200

    amélioration mise en prod

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const PORT = process.env.PORT || process.env.LOCAL_PORT;
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     server: {
         host: true, // permets d'accéder au "serveur" front de n'importe où. Configuration à faire dans un environnement "Cloud9"
-        port: 9500, //nouveau port à utiliser plutôt que le 5173 par défaut
+        port: PORT, //nouveau port à utiliser plutôt que le 5173 par défaut
         proxy: { // c'est une "passerelle"
             '/api': { // toutes les requêtes commençant par /api seront redirigées vers le serveur API (url définit dans le fichier .env de vite)
                 target: process.env.VITE_API_URL,
